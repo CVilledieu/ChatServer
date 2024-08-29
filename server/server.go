b15204d6ed3bbe1d03da58f49461a14462da47a0
Christopher V <ChrisRVill@gmail.com>
2024-08-28 23:51:29 -0700

    No huge changes

--- server/server.go
+++ server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"net/http"
 )
 
@@ -9,5 +12,51 @@ func StartServer() {
 		Addr: ":8080",
 	}
 
+	http.HandleFunc("/", greeting)
+
 	server.ListenAndServe()
+}
+
+func greeting(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func Server() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue
+		}
+
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	request := make([]byte, 1024)
+	_, err := conn.Read(request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	takeInput(request)
+
+	response := []byte("Hello, World!")
+	_, err = conn.Write(response)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
